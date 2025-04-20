19fa4808484e4a6430a98d6e8093417783da40a1
Marcel <marcelalvessc@gmail.com>
2025-04-19 23:08:25 -0300

    fix: Atualizando o CORS

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
     credentials: true,
-    origin: "*",
+    origin: "https://frontend-portfolio-black.vercel.app/",
   })
 );
 
