adbd5c63ca135b375089d1365e89a117f949e1aa
Marcel <marcelalvessc@gmail.com>
2025-04-27 00:32:25 -0300

    fix: Atualizando supabase

--- config/supabaseClient.js
+++ config/supabaseClient.js
@@ -3,6 +3,6 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY;
+const supabaseKey = process.env.SUPABASE_API_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseKey);
